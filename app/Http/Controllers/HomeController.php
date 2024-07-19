f2fea5fdd1b83fa0d0ec68ae926ff982498effa5
Samuel A Fernandes <zamuelfernandes@gmail.com>
2024-07-19 14:49:53 -0300

    GET Parameters and Eloquent

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,7 +12,15 @@ class HomeController extends Controller
     {
         // $allCategories = ['Category 01, Category 02'];
         // $allCategories = DB::table('categories')->get();
-        $allCategories = Category::all();
-        return view('home', ['categories' => $allCategories]);
+        $categories = Category::all();
+        // $posts = Post::orderBy('id', 'desc')->get();
+        // $posts = Post::latest()->get();
+        // $posts = Post::where('category_id', request('category_id'))->latest()->get();
+        $posts = Post::when(request('category_id'), function ($query) {
+            $query->where('category_id', request('category_id'));
+        })->latest()->get();
+
+        // return view('home', ['categories' => $allCategories, 'posts' => $posts,]);
+        return view('home', compact('categories', 'posts'));
     }
 }
